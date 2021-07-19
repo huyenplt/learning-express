a81cb1066a789cec10672cb8a1f46b0b46b90e91
huyenplt <huyen.phamlt19@gmail.com>
2021-07-19 11:40:38 +0700

    cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 
 var userRoute = require('./routes/user.route')
 var port = 3000
@@ -8,8 +9,9 @@ var app = express()
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser())
 
 app.use(express.static('public'))
 
